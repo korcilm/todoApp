9bcce818440e51bf90b9b32166ef5fd4ff07a282
korcilm <58364191+korcilm@users.noreply.github.com>
2020-10-01 18:08:41 +0300

    Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Muhammet';
+  items=[
+    {description:"Kahvaltı" ,action:"No"},
+    {description:"Sinema" ,action:"No"},
+    {description:"Spor" ,action:"No"},
+    {description:"Kitap Okuma" ,action:"No"}
+  ]
 }
